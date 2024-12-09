0a4a864b7e5b349d7f0cf598bc18245b35e800c0
SamerAbiWeb3 <samerweb3@yahoo.comgit config --global user.email samerweb3@yahoo.comgit config --global user.email samerweb3@yahoo.com>
2024-12-09 04:11:46 +0200

    Added handlers and invariants to the testing

---
+++ test/fuzz/OpenInvariantsTest.tt.sol
@@ -0,0 +1,54 @@
+//SPDX-License-Identifier: MIT
+
+/**
+ * What we need to know:
+ *  1. What are our invariants?
+ *      a. The total supple of DSC should be less than the total value of collateral
+ *      b. Getter functions should never revert
+ */
+pragma solidity ^0.8.19;
+
+import {Test, console} from "forge-std/Test.sol";
+import {StdInvariant} from "forge-std/StdInvariant.sol";
+import {DeployDSC} from "../../script/DeployDSC.s.sol";
+import {DSCEngine} from "../../src/DSCEngine.sol";
+import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {Handler} from "./Handler.t.sol";
+
+contract OpenInvariantsTest is StdInvariant, Test {
+    DeployDSC deployer;
+    DSCEngine dsce;
+    DecentralizedStableCoin dsc;
+    HelperConfig config;
+    address weth;
+    address wbtc;
+    Handler handler;
+
+    function setUp() external {
+        deployer = new DeployDSC();
+        (dsc, dsce, config) = deployer.run();
+        (,, weth, wbtc,) = config.activeNetworkConfig();
+
+        handler = new Handler(dsce, dsc);
+        targetContract(address(handler));
+    }
+
+    function Openinvariant_protocolMustHaveMoreValueThanTotalSupply() public view {
+        //get the value of all the collateral in the protocal
+        //compare it to all the debt (dsc)
+        uint256 totalSupply = dsc.totalSupply();
+        uint256 totalWethDeposited = IERC20(weth).balanceOf(address(dsce));
+        uint256 totalBtcDeposited = IERC20(wbtc).balanceOf(address(dsce));
+
+        uint256 wethValue = dsce.getUsdValue(weth, totalWethDeposited);
+        uint256 wbtcValue = dsce.getUsdValue(wbtc, totalBtcDeposited);
+
+        console.log("weth value: %s", wethValue);
+        console.log("wbtc value: ", wbtcValue);
+        console.log("Total Supple: %s", totalSupply);
+
+        assert(wethValue + wbtcValue >= totalSupply);
+    }
+}
